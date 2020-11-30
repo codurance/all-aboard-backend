fcb715d6cb1322a924cbb88f38d7025adc15b3a7
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-11-30 22:59:45 +0100

    Adds custom google token interceptor

---
+++ src/main/java/com/codurance/allaboard/config/GoogleTokenAuthenticator.java
@@ -0,0 +1,43 @@
+package com.codurance.allaboard.config;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class GoogleTokenAuthenticator implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    String token = request.getHeader("Authorization");
+    try {
+      authenticateToken(token, request);
+    } catch (Exception exception) {
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
+      return false;
+    }
+    return true;
+  }
+
+  private void authenticateToken(String token, HttpServletRequest request)
+      throws GeneralSecurityException, IOException {
+    GoogleIdTokenVerifier verifier = buildGoogleIdTokenVerifier();
+    GoogleIdToken idToken = verifier.verify(token);
+    request.getSession().setAttribute("name", idToken.getPayload().get("name"));
+  }
+
+  private GoogleIdTokenVerifier buildGoogleIdTokenVerifier() {
+    return new GoogleIdTokenVerifier
+        .Builder(new NetHttpTransport(), new JacksonFactory())
+        .build();
+  }
+}
---
+++ src/main/java/com/codurance/allaboard/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.codurance.allaboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  private final GoogleTokenAuthenticator googleTokenAuthenticator;
+
+  public WebConfig(GoogleTokenAuthenticator googleTokenAuthenticator) {
+    this.googleTokenAuthenticator = googleTokenAuthenticator;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(googleTokenAuthenticator);
+  }
+}
