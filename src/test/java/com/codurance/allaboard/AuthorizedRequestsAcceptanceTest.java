3587aed1ebfcc954966f43f6e8a740873aec6a39
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-01 12:21:14 +0100

    Adds acceptance tests

---
+++ src/test/java/com/codurance/allaboard/AuthorizedRequestsAcceptanceTest.java
@@ -0,0 +1,45 @@
+package com.codurance.allaboard;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.not;
+
+import java.io.IOException;
+import org.json.JSONException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthorizedRequestsAcceptanceTest {
+
+  @LocalServerPort
+  private int port;
+
+  @Test
+  @Disabled
+  void authenticated_requests_do_not_get_unauthorized_status_code()
+      throws JSONException, IOException {
+
+    given()
+        .header(
+            "Authorization",
+            "YOUR_TOKEN_HERE"
+        )
+        .port(port)
+        .get("/demo")
+        .then()
+        .statusCode(not(HttpStatus.UNAUTHORIZED.value()));
+  }
+
+  @Test
+  @Disabled
+  void unauthenticated_requests_get_unauthorized_status_code() throws JSONException {
+    given()
+        .port(port)
+        .get("/demo")
+        .then()
+        .statusCode(HttpStatus.UNAUTHORIZED.value());
+  }
+}
