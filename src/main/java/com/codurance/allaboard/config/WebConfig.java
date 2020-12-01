4c67692718a6e31d444537e8406b9b957029be9b
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-01 12:18:58 +0100

    Implements up to date interface instead of extending deprecate class

--- src/main/java/com/codurance/allaboard/config/WebConfig.java
+++ src/main/java/com/codurance/allaboard/config/WebConfig.java
@@ -2,10 +2,10 @@ package com.codurance.allaboard.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
   private final GoogleTokenAuthenticator googleTokenAuthenticator;
 
