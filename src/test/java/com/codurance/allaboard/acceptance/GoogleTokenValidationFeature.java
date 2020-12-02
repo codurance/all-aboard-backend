190e65843b1812dacf4068cee2e3576caff7e079
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-02 09:51:45 +0100

    Adds acceptance test
    - Denying requests that don't have Authorization header.

---
+++ src/test/java/com/codurance/allaboard/acceptance/GoogleTokenValidationFeature.java
@@ -0,0 +1,31 @@
+package com.codurance.allaboard.acceptance;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("prod")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GoogleTokenValidationFeature {
+
+  @LocalServerPort
+  private int port;
+
+  @BeforeEach
+  void setUp() {
+    RestAssured.port = port;
+  }
+
+  @Test
+  void deny_requests_without_authorization() {
+    given()
+        .post("/survey")
+        .then()
+        .statusCode(401);
+  }
+}
