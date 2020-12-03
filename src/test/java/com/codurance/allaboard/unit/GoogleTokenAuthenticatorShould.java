649f1534911d0db9ff331405b9e10786709b0f43
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-03 17:40:57 +0100

    Gets user email from token validation
    - Stores it in "user_email" request attribute

--- src/test/java/com/codurance/allaboard/unit/GoogleTokenAuthenticatorShould.java
+++ src/test/java/com/codurance/allaboard/unit/GoogleTokenAuthenticatorShould.java
@@ -2,10 +2,14 @@ package com.codurance.allaboard.unit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 import com.codurance.allaboard.config.security.GoogleTokenAuthenticator;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -15,11 +19,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 @ExtendWith(MockitoExtension.class)
 public class GoogleTokenAuthenticatorShould {
 
-  @Mock
+  private final String email = "user@codurance.com";
+
   private HttpServletRequest request;
 
   @Mock
@@ -31,9 +37,17 @@ public class GoogleTokenAuthenticatorShould {
   @Mock
   private GoogleIdTokenVerifier googleIdTokenVerifier;
 
+  @Mock
+  private GoogleIdToken googleIdToken;
+
+  @Mock
+  private Payload payload;
+
   @Test
   void deny_request_without_authorization() throws GeneralSecurityException, IOException {
     String INVALID_TOKEN = "";
+
+    request = mock(HttpServletRequest.class);
     given(request.getHeader("Authorization"))
         .willReturn(INVALID_TOKEN);
 
@@ -46,8 +60,19 @@ public class GoogleTokenAuthenticatorShould {
   }
 
   @Test
-  void accept_request_with_authorization() {
+  void accept_request_with_authorization() throws GeneralSecurityException, IOException {
     GoogleTokenAuthenticator authenticator = new TestableGoogleTokenAuthenticator();
+
+    request = new ValidTokenHttpServletRequest();
+
+    given(googleIdTokenVerifier.verify(anyString()))
+        .willReturn(googleIdToken);
+
+    given(googleIdToken.getPayload())
+        .willReturn(payload);
+
+    given(payload.getEmail())
+        .willReturn(email);
 
     assertThat(authenticator.preHandle(request, response, handler), is(true));
   }
@@ -57,6 +82,36 @@ public class GoogleTokenAuthenticatorShould {
     @Override
     protected GoogleIdTokenVerifier buildGoogleIdTokenVerifier() {
       return googleIdTokenVerifier;
+    }
+  }
+
+  @Test
+  void set_email_in_request_header() throws GeneralSecurityException, IOException {
+    GoogleTokenAuthenticator authenticator = new TestableGoogleTokenAuthenticator();
+
+    request = new ValidTokenHttpServletRequest();
+
+    given(googleIdTokenVerifier.verify(anyString()))
+        .willReturn(googleIdToken);
+
+    given(googleIdToken.getPayload())
+        .willReturn(payload);
+
+    given(payload.getEmail())
+        .willReturn(email);
+
+    assertThat(request.getAttribute("user_email"), is(nullValue()));
+
+    authenticator.preHandle(request, response, handler);
+
+    assertThat(request.getAttribute("user_email"), is(email));
+  }
+
+  class ValidTokenHttpServletRequest extends MockHttpServletRequest {
+
+    @Override
+    public String getHeader(String name) {
+      return "some valid token";
     }
   }
 }
