2af8b93932e2d1adcde1a03b3b6205013bf811ee
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-07 15:38:46 +0100

    Adds assertions to acceptance test
    - Asserts "GET /learningpaths" response body structure.

--- src/main/resources/stub-catalogue.sql
+++ src/main/resources/stub-catalogue.sql
@@ -1,2 +1,2 @@
-INSERT INTO catalogue VALUES (1, 'first learning path title', 'first learning path description');
-INSERT INTO catalogue VALUES (2, 'second learning path title', 'second learning path description');
+INSERT INTO catalogue (lp_id, lp_name, lp_description) VALUES (1, 'first learning path title', 'first learning path description');
+INSERT INTO catalogue (lp_id, lp_name, lp_description) VALUES (2, 'second learning path title', 'second learning path description');
--- src/test/java/com/codurance/allaboard/web/acceptance/LearningPathsFetchingFeature.java
+++ src/test/java/com/codurance/allaboard/web/acceptance/LearningPathsFetchingFeature.java
@@ -1,10 +1,15 @@
-package com.codurance.allaboard.acceptance;
+package com.codurance.allaboard.web.acceptance;
 
 import io.restassured.RestAssured;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,7 +35,8 @@ public class LearningPathsFetchingFeature {
 
     @Test
     @Sql(scripts = "classpath:stub-catalogue.sql")
-    void given_get_fetch_catalogue() {
+    void given_get_fetch_catalogue() throws IOException {
+        ;
         RequestSpecification httpRequest = httpRequest();
         Response response = httpRequest.get("api/v1/learningpath");
 
@@ -39,6 +45,22 @@ public class LearningPathsFetchingFeature {
 
         assertThat(response.statusCode(), is(200));
         assertThat(learningPaths.length(), is(2));
+        assertThat(responseBody.toString(), is(expectedResponseBody()));
+    }
+
+    private String expectedResponseBody() throws IOException {
+      StringBuilder sb = new StringBuilder();
+      Path filePath = Paths.get("src", "main", "resources", "stub-catalogue.json");
+
+      try (BufferedReader br = Files.newBufferedReader(
+          filePath)) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          sb.append(line);
+        }
+      }
+
+      return sb.toString();
     }
 
     private RequestSpecification httpRequest() {
