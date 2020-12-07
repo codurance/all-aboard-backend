030e8e42f20083ac9fbf8b3f6b68213836cbb9de
Fabio D'Amico <fabio.damico@codurance.com>
2020-12-07 19:33:16 +0000

    :recycle: Moving unit test into his correct package

--- src/test/java/com/codurance/allaboard/core/unit/FetchAllLearningPathsShould.java
+++ src/test/java/com/codurance/allaboard/core/unit/FetchAllLearningPathsShould.java
@@ -1,4 +1,4 @@
-package com.codurance.allaboard.unit;
+package com.codurance.allaboard.core.unit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
