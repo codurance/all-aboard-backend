88780b7c2fafa51a6be155ecdec9c5524464edf3
Fabio D'Amico <fabio.damico@codurance.com>
2020-12-08 13:47:57 +0000

    :art: Format of the code.

--- src/main/java/com/codurance/allaboard/core/model/catalogue/LearningPath.java
+++ src/main/java/com/codurance/allaboard/core/model/catalogue/LearningPath.java
@@ -1,55 +1,59 @@
 package com.codurance.allaboard.core.model.catalogue;
 
-import org.hibernate.annotations.Type;
-
-import javax.persistence.*;
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name = "catalogue")
 public class LearningPath implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="lp_id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "lp_id")
+  private Long id;
 
-    @Column(nullable = false, name="lp_name")
-    private String name;
+  @Column(nullable = false, name = "lp_name")
+  private String name;
 
-    @Type(type="text")
-    @Column(nullable = false, name="lp_description")
-    private String description;
+  @Type(type = "text")
+  @Column(nullable = false, name = "lp_description")
+  private String description;
 
-    public LearningPath(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+  public LearningPath(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
 
-    public LearningPath() {
-    }
+  public LearningPath() {
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
