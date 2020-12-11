bf4d26bdba75f2b4ff109ee152f7c1d62d056910
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-11 14:55:10 +0100

    Adds new acceptance test

---
+++ src/main/java/com/codurance/allaboard/core/actions/topic/FetchTopicById.java
@@ -0,0 +1,20 @@
+package com.codurance.allaboard.core.actions.topic;
+
+import com.codurance.allaboard.core.model.topic.Topic;
+import com.codurance.allaboard.core.model.topic.Topics;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FetchTopicById {
+
+  private final Topics topics;
+
+  public FetchTopicById(Topics topics) {
+    this.topics = topics;
+  }
+
+  public Optional<Topic> findById(Long id) {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/test/java/com/codurance/allaboard/core/acceptance/FetchTopicByIdFeature.java
@@ -0,0 +1,44 @@
+package com.codurance.allaboard.core.acceptance;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+import com.codurance.allaboard.core.actions.topic.FetchTopicById;
+import com.codurance.allaboard.core.model.topic.Topic;
+import com.codurance.allaboard.core.model.topic.Topics;
+import com.codurance.allaboard.web.controllers.topic.TopicController;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class FetchTopicByIdFeature {
+
+  @Mock
+  private Topics topics;
+  private TopicController topicController;
+  private final long NON_EXISTENT_TOPIC_ID = 9L;
+
+  @BeforeEach
+  void setUp() {
+    topicController = new TopicController(new FetchTopicById(topics));
+  }
+
+  @Test
+  void answers_not_found_if_asked_for_a_nonexistent_topic() {
+    given(topics.findById(NON_EXISTENT_TOPIC_ID))
+        .willReturn(Optional.empty());
+
+    ResponseEntity<Optional<Topic>> responseEntity = topicController.fetchTopicsById(NON_EXISTENT_TOPIC_ID);
+
+    verify(topics, atLeastOnce()).findById(NON_EXISTENT_TOPIC_ID);
+    assertThat(responseEntity.getStatusCode().value(), is(404));
+  }
+}
