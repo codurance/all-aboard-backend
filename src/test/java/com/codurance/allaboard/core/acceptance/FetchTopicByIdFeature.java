d82878f67fda59c309e246ad07885b964d3906d3
MaWoDu <durkiewiczmaciej@gmail.com>
2020-12-11 16:00:52 +0000

    :sparkles: Completed implementation of the REST controller.

--- src/test/java/com/codurance/allaboard/core/acceptance/FetchTopicByIdFeature.java
+++ src/test/java/com/codurance/allaboard/core/acceptance/FetchTopicByIdFeature.java
@@ -4,15 +4,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import com.codurance.allaboard.core.actions.topic.FetchTopicById;
 import com.codurance.allaboard.core.model.topic.Topic;
-import com.codurance.allaboard.core.model.topic.Topics;
 import com.codurance.allaboard.web.controllers.topic.TopicController;
 import java.util.Optional;
+
+import com.codurance.allaboard.web.views.TopicDetailView;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -27,6 +28,7 @@ public class FetchTopicByIdFeature {
 
   private TopicController topicController;
   private final long NON_EXISTENT_TOPIC_ID = 9L;
+  private final long EXISTING_TOPIC_ID = 1L;
 
   @BeforeEach
   void setUp() {
@@ -35,12 +37,24 @@ public class FetchTopicByIdFeature {
 
   @Test
   void answers_not_found_if_asked_for_a_nonexistent_topic() {
-    given(fetchTopicById.findById(NON_EXISTENT_TOPIC_ID))
+    given(fetchTopicById.execute(NON_EXISTENT_TOPIC_ID))
         .willReturn(Optional.empty());
 
-    ResponseEntity<Topic> responseEntity = topicController.fetchTopicsById(NON_EXISTENT_TOPIC_ID);
+    ResponseEntity<TopicDetailView> responseEntity = topicController.fetchTopicsById(NON_EXISTENT_TOPIC_ID);
 
-    verify(fetchTopicById, atLeastOnce()).findById(NON_EXISTENT_TOPIC_ID);
+    verify(fetchTopicById, atLeastOnce()).execute(NON_EXISTENT_TOPIC_ID);
     assertThat(responseEntity.getStatusCode().value(), is(404));
+  }
+
+  @Test
+  void answers_ok_when_found_if_asked_for_a_existing_topic() {
+
+    given(fetchTopicById.execute(EXISTING_TOPIC_ID))
+            .willReturn(Optional.of(new TopicDetailView()));
+
+    ResponseEntity<TopicDetailView> responseEntity = topicController.fetchTopicsById(EXISTING_TOPIC_ID);
+
+    verify(fetchTopicById, atLeastOnce()).execute(EXISTING_TOPIC_ID);
+    assertThat(responseEntity.getStatusCode().value(), is(200));
   }
 }
