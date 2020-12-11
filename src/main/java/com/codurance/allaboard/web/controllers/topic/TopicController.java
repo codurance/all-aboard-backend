954f5b1c7e994c163f1250076bfcfd833136d311
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-11 14:55:10 +0100

    Adds new acceptance test

--- src/main/java/com/codurance/allaboard/web/controllers/topic/TopicController.java
+++ src/main/java/com/codurance/allaboard/web/controllers/topic/TopicController.java
@@ -1,14 +1,27 @@
 package com.codurance.allaboard.web.controllers.topic;
 
+import com.codurance.allaboard.core.actions.topic.FetchTopicById;
+import com.codurance.allaboard.core.model.topic.Topic;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+    private final FetchTopicById fetchTopicById;
+
+    @Autowired
+    public TopicController(FetchTopicById fetchTopicById) {
+        this.fetchTopicById = fetchTopicById;
+    }
+
     @GetMapping("topic/{id}")
-    public ResponseEntity<String> fetchTopicsById() {
-        return ResponseEntity.ok("");
+    public ResponseEntity<Optional<Topic>> fetchTopicsById(@PathVariable long id) {
+        Optional<Topic> optionalTopic = fetchTopicById.findById(id);
+        return ResponseEntity.ok(optionalTopic);
     }
 }
