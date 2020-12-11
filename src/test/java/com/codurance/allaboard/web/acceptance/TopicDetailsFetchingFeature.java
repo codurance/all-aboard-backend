84f8314e3a8b59e14f6f3089aede15c3114f0f44
MaWoDu <durkiewiczmaciej@gmail.com>
2020-12-11 10:53:05 +0000

    :sparkles: Added basic controller for the topics.

---
+++ src/test/java/com/codurance/allaboard/web/acceptance/TopicDetailsFetchingFeature.java
@@ -0,0 +1,37 @@
+package com.codurance.allaboard.web.acceptance;
+
+import com.codurance.allaboard.web.utils.RestAssuredUtils;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TopicDetailsFetchingFeature extends RestAssuredUtils {
+
+    @LocalServerPort
+    private int port;
+
+    private static int EXISTING_TOPIC_ID = 1;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void given_get_can_access_endpoint() {
+        RequestSpecification httpRequest = httpRequest();
+        Response response = httpRequest.get(String.format("api/v1/topic/%s", EXISTING_TOPIC_ID));
+        assertThat(response.statusCode(), is(200));
+    }
+
+}
