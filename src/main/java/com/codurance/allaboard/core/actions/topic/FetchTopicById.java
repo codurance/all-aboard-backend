02b0143edf17300f7458a1209cc87553d3a4a417
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-14 15:06:53 +0100

    Adds action implementation
    - Fetch topic by id

--- src/main/java/com/codurance/allaboard/core/actions/topic/FetchTopicById.java
+++ src/main/java/com/codurance/allaboard/core/actions/topic/FetchTopicById.java
@@ -3,7 +3,7 @@ package com.codurance.allaboard.core.actions.topic;
 import com.codurance.allaboard.core.model.topic.Topic;
 import com.codurance.allaboard.core.model.topic.Topics;
 import java.util.Optional;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +11,12 @@ public class FetchTopicById {
 
   private final Topics topics;
 
+  @Autowired
   public FetchTopicById(Topics topics) {
     this.topics = topics;
   }
 
   public Optional<Topic> execute(Long id) {
-    throw new UnsupportedOperationException();
+    return topics.findById(id);
   }
 }
