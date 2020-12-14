f547cf655e632f902e6cacbda910b42a9ed0638e
MaWoDu <durkiewiczmaciej@gmail.com>
2020-12-14 17:01:14 +0000

    Fully implemented Subtopics and Resources endpoint
    - Added Resource View & Subtopic Detail View
    - Changed Topic Detail View to use Subtopic Detail View
    - Amended the json stub data

---
+++ src/main/java/com/codurance/allaboard/web/views/SubtopicDetailView.java
@@ -0,0 +1,42 @@
+package com.codurance.allaboard.web.views;
+
+import com.codurance.allaboard.core.model.topic.Subtopic;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SubtopicDetailView {
+
+    private long id;
+    private String name;
+    private List<ResourceView> resources;
+
+    public SubtopicDetailView() {
+    }
+
+    private SubtopicDetailView(long id, String name, List<ResourceView> resources) {
+        this.id = id;
+        this.name = name;
+        this.resources = resources;
+    }
+
+    public static SubtopicDetailView from(Subtopic subtopic) {
+        return new SubtopicDetailView(
+            subtopic.getId(),
+            subtopic.getName(),
+            subtopic.getResources().stream().map(ResourceView::from).collect(Collectors.toList())
+        );
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ResourceView> getResources() {
+        return resources;
+    }
+}
