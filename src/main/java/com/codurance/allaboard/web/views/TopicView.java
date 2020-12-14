aad84ba5fb8a96f631c127b90c659f17d70f43dc
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-14 15:30:18 +0100

    Renames view

--- src/main/java/com/codurance/allaboard/web/views/TopicView.java
+++ src/main/java/com/codurance/allaboard/web/views/TopicView.java
@@ -2,20 +2,20 @@ package com.codurance.allaboard.web.views;
 
 import com.codurance.allaboard.core.model.topic.Topic;
 
-public class TopicOverviewView {
+public class TopicView {
 
     private final Long id;
     private final String name;
     private final String description;
 
-    private TopicOverviewView(Long id, String name, String description) {
+    private TopicView(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public static TopicOverviewView from(Topic topic) {
-        return new TopicOverviewView(topic.getId(), topic.getName(), topic.getDescription());
+    public static TopicView from(Topic topic) {
+        return new TopicView(topic.getId(), topic.getName(), topic.getDescription());
     }
 
     public Long getId() {
