accfb273e1242089ddf921354870edf9f244d2a0
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-14 17:14:56 +0100

    Specifies sql and json stub files

--- src/test/resources/stub-topic-subtopics.sql
+++ src/test/resources/stub-topic-subtopics.sql
@@ -1,3 +1,5 @@
 INSERT INTO topics (t_id, t_name, t_description) VALUES (1, 'first topic name', 'first topic description');
+INSERT INTO topics (t_id, t_name, t_description) VALUES (2, 'second topic name', 'second topic description');
 INSERT INTO subtopics(s_id, t_id, s_name) VALUES (1, 1, 'first subtopic of first topic name');
 INSERT INTO subtopics(s_id, t_id, s_name) VALUES (2, 1, 'second subtopic of first topic name');
+INSERT INTO subtopics(s_id, t_id, s_name) VALUES (3, 2, 'first subtopic of second topic name');
