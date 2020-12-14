753808d6f0eed67b2f19675cdedd17b9c8c28310
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-14 17:14:40 +0100

    Implements SubtopicView

--- src/main/java/com/codurance/allaboard/web/views/SubtopicView.java
+++ src/main/java/com/codurance/allaboard/web/views/SubtopicView.java
@@ -4,7 +4,23 @@ import com.codurance.allaboard.core.model.topic.Subtopic;
 
 public class SubtopicView {
 
+  private final long id;
+  private final String name;
+
+  private SubtopicView(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
   public static SubtopicView from(Subtopic subtopic) {
-    return null;
+    return new SubtopicView(subtopic.getId(), subtopic.getName());
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
   }
 }
