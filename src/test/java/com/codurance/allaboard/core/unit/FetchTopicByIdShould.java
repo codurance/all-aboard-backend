66d5649a9d23f5d4cc2f891ec980c49599bead1b
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-14 15:06:53 +0100

    Adds action implementation
    - Fetch topic by id

--- src/test/java/com/codurance/allaboard/core/unit/FetchTopicByIdShould.java
+++ src/test/java/com/codurance/allaboard/core/unit/FetchTopicByIdShould.java
@@ -1,26 +1,36 @@
 package com.codurance.allaboard.core.unit;
 
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
 import com.codurance.allaboard.core.actions.topic.FetchTopicById;
-import com.codurance.allaboard.core.model.topic.Topic;
 import com.codurance.allaboard.core.model.topic.Topics;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-@Disabled
+@ExtendWith(MockitoExtension.class)
 public class FetchTopicByIdShould {
 
     private static final long NONEXISTENT_TOPIC_ID = 9L;
 
-    Topics topics;
     FetchTopicById fetchTopicById;
 
+    @Mock
+    private Topics topics;
+
+
+    @BeforeEach
+    void setUp() {
+        fetchTopicById = new FetchTopicById(topics);
+    }
+
     @Test
-    void not_find_not_existing_topic() {
-        Optional<Topic> topic = fetchTopicById.execute(NONEXISTENT_TOPIC_ID);
-        assertFalse(topic.isPresent());
+    void query_the_repository_for_a_topic_by_id() {
+        fetchTopicById.execute(NONEXISTENT_TOPIC_ID);
+
+        verify(topics, atLeastOnce()).findById(NONEXISTENT_TOPIC_ID);
     }
 }
