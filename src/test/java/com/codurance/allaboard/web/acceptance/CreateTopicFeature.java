70df580b194fc95541584d53ef40780febb0ccb4
Martín García Blanco <programadopormartin@gmail.com>
2020-12-18 15:45:08 +0100

    :recycle: Refactor integration test, use json data instead of create the request manually

--- src/test/java/com/codurance/allaboard/web/acceptance/CreateTopicFeature.java
+++ src/test/java/com/codurance/allaboard/web/acceptance/CreateTopicFeature.java
@@ -9,6 +9,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import java.io.IOException;
 import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -25,37 +26,26 @@ public class CreateTopicFeature extends WebAcceptanceTestTemplate {
   @LocalServerPort
   private int port;
   private JSONObject requestBody;
-  private String name = "name";
-  private String description = "description";
-  private JSONArray subtopics;
-  private JSONObject subtopic;
 
   @BeforeEach
   void setUp() {
     RestAssured.port = port;
     requestBody = new JSONObject();
-    subtopic = new JSONObject();
-    subtopics = new JSONArray();
-    subtopic.put("name", "subtopic name");
-    subtopics.put(new JSONObject().put("name", "subtopic 1 name"));
-    subtopics.put(new JSONObject().put("name", "subtopic  2 name"));
-
   }
 
   @Test
-  void accept_a_valid_topic() {
-    requestBody.put("name", name);
-    requestBody.put("description", description);
-    requestBody.put("subtopics", subtopics);
+  void accept_a_valid_topic() throws IOException {
 
+    JSONObject requestBody = buildJsonObjectFromFile("topic.json");
     RequestSpecification httpRequest = httpRequestWithJSONContentType(requestBody);
     Response response = httpRequest.post(apiV1Endpoint("topic"));
     JSONObject responseBody = buildResponseBody(response);
 
+
     assertThat(response.statusCode(), is(201));
     assertThat(response.contentType(), is(ContentType.JSON.toString()));
-    assertThat(responseBody.get("name"), is(name));
-    assertThat(responseBody.get("description"), is(description));
+    assertThat(responseBody.get("name"), is("topic name"));
+    assertThat(responseBody.get("description"), is("topic description"));
   }
 
 
